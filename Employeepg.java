7a2ebe98c53a1c2e78b1146755214d760be1808f
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-24 12:47:32 +0530

    Added using switch case

--- Employeepg.java
+++ Employeepg.java
@@ -2,38 +2,35 @@ package com.EmployeeProblem;
 
 public class Employeepg {
 
+	public static final int IS_Part_Time = 1;
+    public static final int IS_Full_Time =2;
+    public static final int WAGE_Per_Hr =20;
+    
 	public static void main(String[] args) {
-
-	int IS_Full_Time =1;
-	int IS_Present =1;
-	int WAGE_Per_Hr =20;
-
-	int empHrs =0;
-	int empWage = 0
         
-        double empcheck= Math.floor(Math.random() * 10) % 2;
-        if (empcheck == IS_Full_Time)
-                {
-                	empHrs = 8
-	        	empWage = WAGE_Per_Hr * empHrs;
-        	        System.out.println("Full-time");
-        }
-        else if (empcheck == IS_Part_Time)
-        {
-                	empHrs = 8;
-	        	empWage = WAGE_Per_Hr * empHrs;
-	        	System.out.println("Part-time");
-        }
-	 else 
-	{
-	 	empHrs = 0;
-        	empWage = WAGE_Per_Hr * empHrs;
-	 }     	
-	        
-	        System.out.println("Employee Wage: Rs.  " + empWage);
+        int empHrs = 0;
+        int empWage = 0;
+        
+        int empcheck= (int) Math.floor(Math.random() * 10) % 3;
+        switch(empcheck)
+         {
+         	case IS_Part_Time: {  
+         				empHrs = 4;
+         				System.out.println("Part-time");
+         				break;
+         			}
+         	case IS_Full_Time: {
+         				empHrs = 8;
+         				System.out.println("Full-time");
+         				break;
+         			}
+         	default: empHrs = 0;
+         }
+        empWage = WAGE_Per_Hr * empHrs;
+        System.out.println("Employee Wage: Rs.  " + empWage);
 	}
 
-}		
+}
 
 
 
