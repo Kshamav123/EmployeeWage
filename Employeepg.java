ca3882480cbad466d8c5f8ad5a46faf7332c5e81
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-24 11:49:22 +0530

    Added Welcome page

---
+++ Employeepg.java
@@ -0,0 +1,10 @@
+package com.EmployeeProblem;
+
+public class Employeepg {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation Program on Master branch");
+	}
+
+}
+
+
