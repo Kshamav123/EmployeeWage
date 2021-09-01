5d3e78169d070adf7dee7d24e69d72d81775e7ea
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-01 07:42:46 +0530

    Using classes and objects

--- Employeepg.java
+++ Employeepg.java
@@ -1,43 +1,44 @@
 package com.EmployeeProblem;
 
-public class Employeepg {
+public class UC7 {
+    public static final int Is_Part_Time = 1;
+    public static final int Is_Full_Time = 2;
+    public final int workingDays=20;
+    public final int empRate=20;
+    public final int maxHrs=100;
 
-	public static final int IS_Part_Time = 1;
-    public static final int IS_Full_Time =2;
-    public static final int Wage_Per_Hr =20;
-    public static final int Working_Days = 20;
-    public static final int Work_Hours = 100;
-    
-    public static void main(String[] args) {
+    public UC7()
+
+    {
         
-        int empHrs = 0;
-        int empWage = 0, totalEmpWage = 0, totalWorkHrs = 0, totalWorkingDays =0;
-        
-        while (totalWorkHrs <= 100 && totalWorkingDays < 20)
-        {
-            totalWorkingDays++;
-            int empcheck= (int) Math.floor(Math.random() * 10) % 3;
-            switch(empcheck)
-            {
-                case IS_Part_Time: {  
-                                        empHrs = 8;
-                                        //System.out.println("Full-time");
-                                        break;
-                                    }
-                case IS_Full_Time: {
-                                        empHrs = 4;
-                                        //System.out.println("Part-time");
-                                        break;
-                                    }
-                default: empHrs = 0;
-            }
-            totalWorkHrs += empHrs;
-            empWage = Wage_Per_Hr * empHrs;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage: Rs. " + empWage);
-        }
-        System.out.println("Employee Wage for a month: Rs.  " + totalEmpWage);
     }
 
+    public static void main(String[] args) {
+        UC7 obj = new UC7();
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0, totalWorkHrs = 0, totalWorkingDays = 0;
+        while (totalWorkHrs <= obj.maxHrs && totalWorkingDays < obj.workingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+            case Is_Part_Time: {
+                empHrs = 8;
+                break;
+            }
+            case Is_Full_Time: {
+                empHrs = 4;
+                break;
+            }
+            default:
+                empHrs = 0;
+            }
+            totalWorkHrs +=empHrs;
+            empWage = obj.empRate * empHrs; 
+            totalEmpWage += empWage;
+            System.out.println("Day " + totalWorkingDays + " ");
+            System.out.println("Employee Wage for Company : Rs."  + empWage);
+        }
 
+    }
 }
