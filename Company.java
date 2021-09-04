350fb74928964d5c9d30f1c6fa3ac7d894ff057a
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-04 21:54:10 +0530

    Adding company class

---
+++ Company.java
@@ -0,0 +1,30 @@
+package com.EmployeeProblem;
+
+public class Company {
+	 public final String company;  
+	 public final int empRate;     
+	 public final int workingDays; 
+	 public final int maxHrs;      
+	 public int totalWage;
+	
+	public Company(String company,int empRate,int workingDays,int maxHrs)
+	{
+		this.company=company;
+		this.empRate = empRate;
+		this.workingDays = workingDays;
+		this.maxHrs = maxHrs;
+	}
+	
+	public void setTotalEmpWage(int totalWage)
+	{
+		this.totalWage=totalWage;
+	}
+	
+	@Override
+	public String toString() {
+		return "Total Employee Wage of the company "+company+ " is: Rs. "+totalWage;
+				} 
+
+}
+
+
