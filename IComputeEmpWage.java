9e86c21bf7bc82f1c51d13a45499ae0891200996
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-06 16:56:06 +0530

    Added Total Wage whe queried by companyn

--- IComputeEmpWage.java
+++ IComputeEmpWage.java
@@ -1,6 +1,9 @@
 package com.EmployeeProblem;
 
 public interface IComputeEmpWage {
-	public void addCompanyEmpWage(String company,int empRate,int workingDays,int maxHrs);
+	public void addCompanyEmpWage(String company, int empRate, int workingDays, int maxHrs);
+
 	public void computeEmpWage();
+
+	public int getTotalEmpWage(String company);
 }
